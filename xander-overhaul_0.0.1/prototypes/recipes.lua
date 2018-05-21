1f48caf1f3588f44568f40816a1e4044f57817a7
Yamagato <dr.dragent@gmail.com>
2018-05-21 22:46:17 +0100

    Comments

--- xander-overhaul_0.0.1/prototypes/recipes.lua
+++ xander-overhaul_0.0.1/prototypes/recipes.lua
@@ -1,3 +1,7 @@
+--All crafting recipes included in this file. For readability sake, please keep the order of commands more or less in the order which you'd find in-game.
+--EG: Iron Extraction followed by Iron processing followed by Iron gear recipe.
+
+--Make sure you don't assume the pressence of any mod. We have to make each step have alternative optainability in case some mod isn't present. Under no circumstances do we want to not provide a gears crafting recipe. That would break the progression.
 if mods["omnimatter"] then
 	if mods["bobores"] then
 		if mods["angelssmelting"] then
@@ -10,14 +14,14 @@ if mods["omnimatter"] then
 	end
 end
 if mods["bobores"] then
-	addToKilnSmelting({name="tin-ore",amount=5}, {name="plate-rudimentary-tin", amount=1}, "bobores")
-	addToKilnSmelting({name="iron-ore",amount=5}, {name="plate-rudimentary-iron", amount=1}, "bobores")
-	addToKilnSmelting({name="copper-ore",amount=5}, {name="plate-rudimentary-copper", amount=1}, "bobores")
-	addToKilnSmelting({name="lead-ore",amount=5}, {name="plate-rudimentary-lead", amount=1}, "bobores")
+	addToKilnSmelting({name="tin-ore",amount=5}, {name="plate-impure-tin", amount=1}, "bobores")
+	addToKilnSmelting({name="iron-ore",amount=5}, {name="plate-impure-iron", amount=1}, "bobores")
+	addToKilnSmelting({name="copper-ore",amount=5}, {name="plate-impure-copper", amount=1}, "bobores")
+	addToKilnSmelting({name="lead-ore",amount=5}, {name="plate-impure-lead", amount=1}, "bobores")
 end
 
-if omni.lib.does_exist("plate-rudimentary-tin-kiln-smelting") and omni.lib.does_exist("plate-rudimentary-copper-kiln-smelting") then
-	addToKilnSmelting({{name="plate-rudimentary-tin",amount=2},{name="plate-rudimentary-copper",amount=6}},{name="plate-rudimentary-bronze", amount=1}, "xander-overhaul")
+if omni.lib.does_exist("plate-impure-tin-kiln-smelting") and omni.lib.does_exist("plate-impure-copper-kiln-smelting") then
+	addToKilnSmelting({{name="plate-impure-tin",amount=2},{name="plate-impure-copper",amount=6}},{name="plate-impure-bronze", amount=1}, "xander-overhaul")
 end
 
 if mods["omnimatter"] then
