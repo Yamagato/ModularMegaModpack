ea3df4dc23f0c98c369af9c470911bd217db59fe
Yamagato <dr.dragent@gmail.com>
2018-05-21 15:18:11 +0100

    Fixed folders
    Put all loose files into xander-overhaul_0.0.1

--- prototypes/items.lua
+++
@@ -1,7 +0,0 @@
-require("itemFunctions")
-addItem("iron-plate-rudimentary", "metal")
-addItem("copper-plate-rudimentary", "metal")
-addItem("tin-plate-rudimentary", "metal")
-addItem("lead-plate-rudimentary", "metal")
-
-finalizeItems()
