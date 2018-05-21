8efd46f08b40e673be7ca3a4092c2592d4db9115
Yamagato <dr.dragent@gmail.com>
2018-05-21 22:36:06 +0100

    Update assemblyMachineFunctions.lua

--- modularlib_0.0.1/assemblyMachineFunctions.lua
+++ modularlib_0.0.1/assemblyMachineFunctions.lua
@@ -1,3 +1,6 @@
+--TODO: Finish implementation
+
+--This function will create a crafting machine (Assembly Machine, Furnace, etc)
 function createAssemblingMachine(machineName, craftingCategories, modName)
 	
 end
